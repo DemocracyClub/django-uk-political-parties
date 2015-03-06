8fb1c6781a81a0229f702e463d067bbc4d1ad671
Sym Roe <sym.roe@talusdesign.co.uk>
2015-03-06 18:05:25 +0000

    Improve the name matcher

--- uk_political_parties/models.py
+++ uk_political_parties/models.py
@@ -8,87 +8,105 @@ from django_extensions.db.fields import  AutoSlugField
 
 class PartyManger(models.Manager):
 
+    def _alternative_name_guesses(self, name):
+        """
+        Returns a list of names to try, including removing common errors
+        and different ways of spelling them (i.e., removing 'The' form the
+        start of the name)
+        """
+        return [
+            name,
+            re.sub(r"([^\[]+)\[The\]", r"The \1", name).strip(),
+            re.sub(r"([^\[]+)\[The\]", r"\1", name).strip(),
+            re.sub(r"^The(.*)", r"\1", name).strip(),
+            re.sub(r"\([^\)]+\)", r"", name).strip(),
+            re.sub(r"United Kingdom", r"UK", name).strip(),
+        ]
+
+    def _clean_known_names(self, name):
+        if name == "Animals Count":
+            return "Animal Welfare Party"
+        if name == "Independent Community and Health Concern":
+            return "Independent Kidderminster Hospital and Health Concern"
+        if name == "United Kingdom Independence Party":
+            return "UK Independence Party (UK I P)"
+        if name == "Independent or Independent Network":
+            return "Independent"
+        if name == u"National Liberal Party, Third Way":
+            return "Ulster Third Way"
+        if name == u"National Liberal Party, The Third Way":
+            return "Ulster Third Way"
+        if name == "Plaid Cymru - Party of Wales":
+            return "Plaid Cymru - The Party of Wales"
+        if name == "Plaid Cymru - Party of Wales [The]":
+            return "Plaid Cymru - The Party of Wales"
+        if name == "Trade Unionist and SocialistCoalition":
+            return "Trade Unionist and Socialist Coalition"
+        if name == "WAI D":
+            return "Your Decision"
+        if name == "Movement for Active Democracy! (M.A.D!)":
+            return "Movement for Active Democracy (M.A.D.)"
+        if name == "Scottish National Party":
+            return "Scottish National Party (SNP)"
+        if name == "Sinn Fein":
+            return "Sinn Féin"
+        if name == "Yes to AV":
+            return "Yes in May 2011 Ltd"
+        return name
+
+    def _match_name(self, name, party_type, register):
+        # import ipdb; ipdb.set_trace()
+        matches = self.filter(
+            party_name__iexact=name,
+            party_type=party_type,
+            register=register
+            ).order_by('-weight')
+        if matches:
+            return matches[0]
+
+
     def find_party_by_name(self, name):
         """
-        Given a name, try to match to a party, using increasingly hacky methods.
+        Given a name, try to match to a party, using increasingly
+        hacky methods.
 
         Returns the first party found with a reasonably similar name.
 
         Searches from large UK wide parties to small parties, and picks active
         parties over deregistered ones.
         """
+        name = self._clean_known_names(name)
+        if name.lower().strip() in ["unknown party",
+                    "other",
+                    "independent",
+                    "the speaker seeking re-election",
+                    "no to av",
+                ]:
+            return None
 
-        def _match_name(name):
-            # name = name.lower()
-            matches = self.filter(party_name__iexact=name)
-            if matches:
-                if len(matches) > 1:
-                    matches = matches.filter(party_type='Political Party')
-                    if len(matches) > 1:
-                        matches = matches.filter(register='Great Britain')
-                        if len(matches) > 1:
-                            matches = matches.filter(status='Authorised')
-                            if len(matches) > 1:
-                                print matches
-                if len(matches) == 1:
-                    return matches[0]
-                else:
-                    print "ERROR More than one match, ", matches
+        party_search_order = [
+            "Political Party",
+            "Minor Party",
+            "Permitted Participant",
+            "Third Party",
+        ]
 
-            matches = self.filter(party_name__istartswith=name)
-            if matches:
-                name = matches[0].party_name
-                match = _match_name(name)
-                if match:
-                    return match
-            if len(matches) == 1:
-                return matches[0]
+        register_search_order = [
+            "Great Britain",
+            "Northern Ireland",
+            None
+        ]
 
-            matches = self.filter(party_name__contains=name)
-            if matches:
-                match = _match_name(matches[0].party_name)
-                if match:
-                    return match
-
-        def _clean_known_names(name):
-            if name == "Animals Count":
-                return "Animal Welfare Party"
-            if name == "Independent Community and Health Concern":
-                return "Independent Kidderminster Hospital and Health Concern"
-            if name == "Independent or Independent Network":
-                return "Independent"
-            if name == u"National Liberal Party, Third Way":
-                print "FOUND"
-                return "Ulster Third Way"
-            if name == u"National Liberal Party, The Third Way":
-                return "Ulster Third Way"
-            if name == "Plaid Cymru - Party of Wales":
-                return "Plaid Cymru - The Party of Wales"
-            if name == "Plaid Cymru - Party of Wales [The]":
-                return "Plaid Cymru - The Party of Wales"
-            if name == "Trade Unionist and SocialistCoalition":
-                return "Trade Unionist and Socialist Coalition"
-            if name == "WAI D":
-                return "Your Decision"
-            if name == "Movement for Active Democracy! (M.A.D!)":
-                return "Movement for Active Democracy (M.A.D.)"
-            if name == "Sinn Fein":
-                return "Sinn Féin"
-            if name == "Yes to AV":
-                return "Yes in May 2011 Ltd"
-            return name
-
-        name = _clean_known_names(name)
-        match = _match_name(name)
+        names = self._alternative_name_guesses(name)
+        match = None
+        for register in register_search_order:
+            for party_type in party_search_order:
+                for name in names:
+                    match = self._match_name(name, party_type, register)
+                    if match:
+                        return match
         if not match:
-            old_name = name
-            name = re.sub(r"([^\[]+)\[The\]", r"The \1", name).strip()
-            match = _match_name(name)
-            if not match:
-                name = re.sub(r"^The(.*)", r"\1", name).strip()
-                match = _match_name(name)
-        if match:
-            return match
+            raise ValueError
 
 
 class PartyEmblem(models.Model):
