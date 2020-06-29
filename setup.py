5fb45ade0d7bc69354d4d8ba87fadec18ab28f35
Sym Roe <sym.roe@talusdesign.co.uk>
2020-06-29 11:52:55 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-uk-political-parties',
-    version='0.2',
+    version='0.3',
     packages=['uk_political_parties'],
     include_package_data=True,
     license='BSD License',  # example license
